09bfd12d14e0547b49717b6126005181b3e18a6d
Mabelys Arias <MabelysA@gmail.com>
2021-07-21 11:12:36 -0400

    adds happy/sad paths to controller and validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,43 @@
 class PlacesController < ApplicationController
+  def index
+    places = Place.all
+    render json: places.as_json
+  end
+
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address],
+    )
+    if place.save
+      render json: place.as_json
+    else
+      render json: { errors: place.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def show
+    place = Place.find_by(id: params[:id])
+    render json: place.as_json
+  end
+
+  def update
+    place = Place.find_by(id: params[:id])
+    place.name = params[:name] || place.name
+    place.address = params[:address] || place.address
+
+    if place.save
+      render json: place.as_json
+    else
+      render json: { errors: place.errors.full_messages },
+             status: 422
+    end
+  end
+
+  def destroy
+    place = Place.find_by(id: params[:id])
+    place.destroy
+    render json: { message: "Successfully destroyed!" }
+  end
 end
